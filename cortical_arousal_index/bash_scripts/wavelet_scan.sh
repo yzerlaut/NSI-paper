8c97971c8de0af5d76a0d01d344ac00d7adbc512
yzerlaut <yann.zerlaut@gmail.com>
2018-04-05 14:38:51 +0200

    corrected scan

--- cortical_arousal_index/bash_scripts/wavelet_scan.sh
+++ cortical_arousal_index/bash_scripts/wavelet_scan.sh
@@ -1,4 +1,4 @@
 # first the wavelet scan
-python exp_data.py -tdw -f data/trial30.npz --wavelet_discretization 30 --parallelize --subsampling_period 1e-3 
+python exp_data.py -tdw -f data/trial30.npz --discretization 30 --parallelize --subsampling_period 1e-3 
 # then the smoothing scan
 python exp_data.py -tds -if data/trial30.npz -f data/smooth30.npz --parallelize --discretization 40 --subsampling_period 1e-3 
