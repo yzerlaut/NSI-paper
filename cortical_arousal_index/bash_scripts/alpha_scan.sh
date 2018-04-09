5a6233d1871e8a8a1ef4898dfd8332c4982b0cd6
yzerlaut <yann.zerlaut@gmail.com>
2018-04-09 15:14:39 +0200

    ==================== \n if commit works you forgot to commit !!! \n ==========================

--- cortical_arousal_index/bash_scripts/alpha_scan.sh
+++ cortical_arousal_index/bash_scripts/alpha_scan.sh
@@ -1,2 +1,2 @@
 # first the wavelet scan
-python exp_data.py -tda -f data/alpha40.npz --parallelize --discretization 40 --subsampling_period 1e-3 --args.alpha_min 1. --alpha_max 7.
+python exp_data.py -tda -f data/alpha40.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7.
