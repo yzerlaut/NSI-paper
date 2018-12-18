1bca8ecd6569e3b74f43731ad05220c991024d80
Yann Zerlaut <yann.zerlaut@gmail.com>
2018-12-18 21:07:55 +0100

    uupdate paper

--- cortical_arousal_index/bash_scripts/alpha_scan.sh
+++ cortical_arousal_index/bash_scripts/alpha_scan.sh
@@ -1,2 +1,2 @@
-# first the wavelet scan
-python exp_data.py -tda -f data/alpha40.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7.
+# the alpha scan
+python exp_data.py -tda -f data/final_alpha.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7.
