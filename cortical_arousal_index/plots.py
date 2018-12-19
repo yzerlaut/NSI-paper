d5491d0c30732d902a5f46d7177bd6173d3ca006
Yann Zerlaut <yann.zerlaut@gmail.com>
2018-12-19 14:41:03 +0100

    adding the old version of show_data_large_scale

--- cortical_arousal_index/plots.py
+++ cortical_arousal_index/plots.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pylab as plt
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 from data_analysis.processing.signanalysis import gaussian_smoothing
+# COLORS
+NSI_color, Vm_color, LFP_color, pLFP_color = Brown, 'k', Grey, Brown
+Rhythm_color, Asynch_color = Purple, Kaki
+
 
 from graphs.my_graph import *
 
@@ -87,3 +91,55 @@ def show_raw_data_Vm_Vext(data,
     set_plot(ax, [], xlim=[tstart, tstart+twidth], ylim=ylim)
     
     return fig, ax
+
+def show_raw_data_large_scale(data,
+                              Zooms=[],
+                              Tbar=5, Vm_bar=30, Vm_bar_loc=(0,0),
+                              LFP_bar=0.7, LFP_loc=-5,
+                              NSI_bar=10, NSI_bar_loc=(0,10),
+                              tstart=0, twidth=np.inf,
+                              fig_options={'figsize':(0.8,.1), 'hspace':0.01, 'left':.12, 'bottom':0.01, 'right':.999},
+                              subsampling=400, spikes=40):
+    tend = tstart+twidth
+    fig, AX = figure(axes_extents=[[[1,3]], [[1,2]]], **fig_options)
+    
+    AX[0][0].plot(Vm_bar_loc[0]*np.ones(2)+tstart, Vm_bar_loc[1]+np.arange(2)*Vm_bar, '-', lw=1, color=Vm_color)
+    AX[0][0].annotate(str(Vm_bar)+'mV', (Vm_bar_loc[0]+tstart, Vm_bar_loc[1]), fontsize=FONTSIZE, color=Vm_color)
+    AX[0][0].annotate(str(round(LFP_bar,1))+'mV', (Vm_bar_loc[0]+tstart, Vm_bar_loc[1]+Vm_bar), fontsize=FONTSIZE, color=LFP_color)
+    AX[0][0].annotate('$V_m$', (-0.04,0.1), fontsize=FONTSIZE, color=Vm_color, xycoords='axes fraction')
+    AX[0][0].annotate('$V_{ext}$', (-0.04,0.6), fontsize=FONTSIZE, color=LFP_color, xycoords='axes fraction')
+    AX[1][0].plot(NSI_bar_loc[0]*np.ones(2)+tstart, NSI_bar_loc[1]+np.arange(2)*NSI_bar, 'k-', lw=1)
+    AX[1][0].annotate(str(NSI_bar)+'$\mu$V', (NSI_bar_loc[0]+tstart, NSI_bar_loc[1]), fontsize=FONTSIZE)#, color=NSI_color)
+    AX[1][0].annotate('NSI', (-0.04,0.3), fontsize=FONTSIZE, color=NSI_color, xycoords='axes fraction')
+    AX[0][0].plot([tstart,tstart+twidth], data['p0_Vm']*np.ones(2), ':', lw=0.5, color=Vm_color)
+    AX[0][0].plot([tstart,tstart+twidth], LFP_loc*np.ones(2), ':', lw=0.5, color=LFP_color)
+    
+    cond = (data['t']>tstart) & (data['t']<tend)
+    AX[0][0].plot(data['t'][cond][::subsampling], data['Vm'][cond][::subsampling], '-', lw=1, color=Vm_color)
+    AX[0][0].plot(data['t'][cond][::subsampling],\
+                  (data['Extra'][cond][::subsampling]-data['Extra'][cond].mean())/LFP_bar*Vm_bar+LFP_loc,
+                  color=LFP_color, lw=1)
+    ispk = np.argwhere(cond[1:] & (data['Vm'][:-1]<=spikes) & (data['Vm'][1:]>spikes) & (data['t'][1:]>tstart)).flatten()
+    for i in ispk:
+        Vspk = data['Vm'][i-10:i+30]
+        AX[0][0].plot(data['t'][i-10:i+30][Vspk<spikes], Vspk[Vspk<spikes], '-', lw=1, color=Vm_color)
+    # tstart = data['t'][cond][0]
+    cond0 = (data['sbsmpl_t']>tstart) & (data['sbsmpl_t']<tend)
+    AX[1][0].plot(data['sbsmpl_t'][cond0][data['NSI_validated'][cond0]],\
+                  data['NSI'][cond0][data['NSI_validated'][cond0]], 'o', lw=0, ms=1, color=NSI_color)
+    y20, y21 = AX[1][0].get_ylim()
+    y10, y11 = AX[0][0].get_ylim()
+    AX[1][0].annotate(str(Tbar)+'s', (data['t'][cond][0]+Tbar, y20), fontsize=FONTSIZE)
+    AX[1][0].plot(data['t'][cond][0]+np.arange(2)*Tbar, y20*np.ones(2), 'k-', lw=1)
+    for i, (z1, z2) in enumerate(Zooms):
+        AX[0][0].annotate('('+str(i+1)+')', (z1, y11), fontsize=FONTSIZE, color=Grey)
+        AX[1][0].fill_between([z1, z2], y20*np.ones(2), y21*np.ones(2), color='k', alpha=.1, lw=0)
+        AX[0][0].fill_between([z1, z2], y10*np.ones(2), y11*np.ones(2), color='k', alpha=.1, lw=0)
+    AX[1][0].plot([data['t'][cond][0],data['t'][cond][-1]], np.zeros(2), 'k:', lw=0.5)
+    AX[1][0].fill_between([data['t'][cond][0],data['t'][cond][-1]], np.zeros(2), np.ones(2)*y20,
+                              color=Rhythm_color, alpha=.2, lw=0)
+    AX[1][0].fill_between([data['t'][cond][0],data['t'][cond][-1]], np.zeros(2), np.ones(2)*y21,
+                              color=Asynch_color, alpha=.2, lw=0)
+    set_plot(AX[0][0], [],xlim=[data['t'][cond][0],data['t'][cond][-1]])
+    set_plot(AX[1][0], [],xlim=[data['t'][cond][0],data['t'][cond][-1]])
+    return fig, AX
