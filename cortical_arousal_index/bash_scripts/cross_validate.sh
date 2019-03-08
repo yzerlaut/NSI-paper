8653baeb049fc7782355028b2f043e88b6047c79
Yann Zerlaut <yann.zerlaut@gmail.com>
2019-03-08 15:26:53 +0100

    still need to include the supplementary

--- cortical_arousal_index/bash_scripts/cross_validate.sh
+++ cortical_arousal_index/bash_scripts/cross_validate.sh
@@ -1,5 +1,5 @@
 # the cross-validation script
 # python exp_data.py -tda -f data/final_alpha_first_third.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7. --data_fraction 0. 0.33 &
 # python exp_data.py -tda -f data/final_alpha_last_third.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7. --data_fraction 0.66 1. &
-python exp_data.py -tda -f data/final_alpha_first_half.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7. --data_fraction 0. 0.5 &
-# python exp_data.py -tda -f data/final_alpha_last_half.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7. --data_fraction 0.5 1. &
+# python exp_data.py -tda -f data/final_alpha_first_half.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7. --data_fraction 0. 0.5 &
+python exp_data.py -tda -f data/final_alpha_last_half.npz --parallelize --discretization 40 --subsampling_period 1e-3 --alpha_min 1. --alpha_max 7. --data_fraction 0.5 1. &
